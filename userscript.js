5757082d533010df60f43273981e06b22546a38b
MineMooreALT <thewindmillgamer@gmail.com>
2016-10-05 11:26:17 +0800

    123334
    12334

---
+++ userscript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         MineMoore Bots - Client
+// @match        http://agar.io/*
+// @run-at       document-start
+// ==/UserScript==
+function loadScript(a) {
+    var b = document.createElement("script");
+    b.type = "text/javascript";
+    b.src = a;
+    document.head.appendChild(b);
+}
+
+function stopPage() {
+    window.stop();
+    document.documentElement.innerHTML = null;
+}
+stopPage();
+loadScript("https://code.jquery.com/jquery-3.1.0.min.js");
+loadScript("https://cdn.socket.io/socket.io-1.4.5.js");
+loadScript("https://raw.githubusercontent.com/MineMooreALT/MineMooreALT.github.io/nigga/USER.js");
